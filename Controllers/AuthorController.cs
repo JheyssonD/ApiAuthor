90861662084543577a76cb0e5d0b7bbc58fe85d6
Jheysson Diaz <jdiaz_ext@dataifx.com>
2021-07-20 12:18:52 -0400

    Added Author Controller added Author Entity

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using ApiAuthor.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAuthor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Author>> Get()
+        {
+            return new List<Author> {
+                new Author() { Id= 1, Name="Felipe"},
+                new Author() { Id= 2, Name="Claudia"}
+            };
+        }
+
+    }
+}
