5b24d6f90c25c1e855dafb49144f31acc4be99a5
Jheysson Diaz <jdiaz_ext@dataifx.com>
2021-09-08 14:07:07 -0400

    29 Model Validations

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -8,22 +8,47 @@ using System.Threading.Tasks;
 
 namespace ApiAuthor.Entities
 {
-    public class Author
+    public class Author : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El Campo {0} es requerido")]
         [StringLength(maximumLength: 4, ErrorMessage = "El Campo {0} no debe tener mas de {1} carácteres")]
-        [FirstCapitalLetter]
+        // [FirstCapitalLetter]
         public string Name { get; set; }
-        [Range(18, 120)]
+        //[Range(18, 120)]
+        //[NotMapped]
+        //public int Age { get; set; }
+        //[CreditCard]
+        //[NotMapped]
+        //public string CredicardNumber{ get; set; }
+        //[Url]
+        //[NotMapped]
+        //public string Site { get; set; }
         [NotMapped]
-        public int Age { get; set; }
-        [CreditCard]
+        public int LessThan { get; set; }
         [NotMapped]
-        public string CredicardNumber{ get; set; }
-        [Url]
-        [NotMapped]
-        public string Site { get; set; }
+        public int GreaterThan { get; set; }
+
         public virtual List<Book> Books { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                string firstLetter = Name.ToString()[0].ToString();
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("The first Letter must be Capital", 
+                        new String[] { nameof(Name) });
+                }
+
+            }
+
+            if (LessThan > GreaterThan)
+            {
+                yield return new ValidationResult("This field can't be greater than the GreaterThan field",
+                    new String[] { nameof(LessThan) });
+            }
+        }
     }
 }
