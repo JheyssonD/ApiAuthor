56d051056a2bcef9102fca84228d671cb65fec6b
Jheysson Diaz <jdiaz_ext@dataifx.com>
2021-09-08 13:36:30 -0400

    26, 27 Model Bindng validation model Default Validations

--- Entities/Author.cs
+++ Entities/Author.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,18 @@ namespace ApiAuthor.Entities
     public class Author
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El Campo {0} es requerido")]
+        [StringLength(maximumLength: 4, ErrorMessage = "El Campo {0} no debe tener mas de {1} carácteres")]
         public string Name { get; set; }
+        [Range(18, 120)]
+        [NotMapped]
+        public int Age { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string CredicardNumber{ get; set; }
+        [Url]
+        [NotMapped]
+        public string Site { get; set; }
         public virtual List<Book> Books { get; set; }
     }
 }
